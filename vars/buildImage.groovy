4a725d52f6a4e81b4cd868b288c79ba75030895f
Kevin Pirner <kevin.pirner@openforce.com>
2024-02-20 17:02:56 +0100

    add parameters for build name

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t kvnvna/demo-app:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push kvnvna/demo-app:jma-2.0'
+        sh "docker push $imageName"
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo 'building the application...'
+    echo "building the application for branch $BRANCH_NAME"
     sh 'mvn package'
 }
